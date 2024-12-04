786b6b61377022a714a55aa7cc3d3d18f0503c48
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-04 09:44:04 +0800

    Update types_handler.py

--- types_handler.py
+++ types_handler.py
@@ -55,16 +55,38 @@ def create_types_table(cursor):
             metaGroupID INTEGER,
             iconID INTEGER,
             groupID INTEGER,
+            group_name TEXT,
+            category_name TEXT,
             pg_need INTEGER,
             cpu_need INTEGER
         )
     ''')
 
 
+def fetch_and_process_data(cursor):
+    # 查询 categories 表
+    cursor.execute("SELECT category_id, name FROM categories")
+    categories_data = cursor.fetchall()
+
+    # 查询 groups 表
+    cursor.execute("SELECT group_id, name, categoryID FROM groups")
+    groups_data = cursor.fetchall()
+
+    # 1. 创建 category_id 与 group_id 的关系
+    group_to_category = {group_id: category_id for group_id, _, category_id in groups_data}
+
+    # 2. 创建 categories 中 category_id 与 name 的映射关系
+    category_id_to_name = {category_id: name for category_id, name in categories_data}
+
+    # 3. 创建 groups 中 group_id 与 name 的映射关系
+    group_id_to_name = {group_id: name for group_id, name, _ in groups_data}
+
+    return group_to_category, category_id_to_name, group_id_to_name
+
 def process_data(types_data, cursor, lang):
     """处理 types 数据并插入数据库（针对单一语言）"""
     create_types_table(cursor)
-
+    group_to_category, category_id_to_name, group_id_to_name = fetch_and_process_data(cursor)
     for item_id, item in types_data.items():
         name = item['name'].get(lang, item['name'].get('en', ""))  # 优先取 lang，没有则取 en
         description = item.get('description', {}).get(lang,
@@ -75,6 +97,9 @@ def process_data(types_data, cursor, lang):
         metaGroupID = item.get('metaGroupID', 1)
         iconID = item.get('iconID', 0)
         groupID = item.get('groupID', 0)
+        group_name = group_id_to_name.get(groupID, 'Unknown')
+        category_id = group_to_category.get(groupID, 0)
+        category_name = category_id_to_name.get(category_id, 'Unknown')
         copied_file = copy_and_rename_icon(item_id)  # 复制物品图像
         # 获取 pg_need 和 cpu_need 的值
         pg_need = get_attribute_value(cursor, item_id, 30)  # 获取 pg占用 的值 (pg_need)
@@ -82,10 +107,10 @@ def process_data(types_data, cursor, lang):
 
         # 使用 INSERT OR IGNORE 语句，避免重复插入
         cursor.execute('''
-            INSERT OR IGNORE INTO types (type_id, name, description, icon_filename, published, volume, marketGroupID, metaGroupID, iconID, groupID, pg_need, cpu_need)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT OR IGNORE INTO types (type_id, name, description, icon_filename, published, volume, marketGroupID, metaGroupID, iconID, groupID, group_name, category_name, pg_need, cpu_need)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-            item_id, name, description,copied_file,  published, volume, marketGroupID, metaGroupID, iconID, groupID, pg_need,
+            item_id, name, description,copied_file,  published, volume, marketGroupID, metaGroupID, iconID, groupID, group_name,category_name, pg_need,
             cpu_need))
 
 
