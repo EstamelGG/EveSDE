a537e6a0adba201ed468ca72ab3076c316817a0c
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-13 09:43:53 +0800

    Update typeMaterials_handler.py

--- typeMaterials_handler.py
+++ typeMaterials_handler.py
@@ -14,6 +14,7 @@ def process_data(yaml_data, cursor, language):
         categoryid INTEGER,
         process_size INTEGER,
         output_material INTEGER,
+        output_material_categoryid INTEGER,
         output_quantity INTEGER,
         output_material_name TEXT,
         output_material_icon TEXT,
@@ -24,49 +25,47 @@ def process_data(yaml_data, cursor, language):
     # 清空现有数据
     cursor.execute('DELETE FROM typeMaterials')
     
-    # 创建材料信息缓存字典
-    material_info = {}
+    # 创建物品信息缓存字典
     type_info_cache = {}
     
-    def get_material_info(material_id):
-        """从缓存或数据库获取材料信息"""
-        if material_id not in material_info:
-            cursor.execute('SELECT name, icon_filename FROM types WHERE type_id = ?', (material_id,))
-            result = cursor.fetchone()
-            if result:
-                material_info[material_id] = {'name': result[0], 'icon': result[1]}
-            else:
-                material_info[material_id] = {'name': None, 'icon': None}
-        return material_info[material_id]
-    
     def get_type_info(type_id):
-        """从缓存或数据库获取物品的categoryid和process_size"""
+        """从缓存或数据库获取物品的所有相关信息"""
         if type_id not in type_info_cache:
-            cursor.execute('SELECT categoryID, process_size FROM types WHERE type_id = ?', (type_id,))
+            cursor.execute('SELECT name, icon_filename, categoryID, process_size FROM types WHERE type_id = ?', (type_id,))
             result = cursor.fetchone()
             if result:
-                type_info_cache[type_id] = {'categoryid': result[0], 'process_size': result[1]}
+                type_info_cache[type_id] = {
+                    'name': result[0],
+                    'icon': result[1],
+                    'categoryid': result[2],
+                    'process_size': result[3]
+                }
             else:
-                type_info_cache[type_id] = {'categoryid': None, 'process_size': None}
+                type_info_cache[type_id] = {
+                    'name': None,
+                    'icon': None,
+                    'categoryid': None,
+                    'process_size': None
+                }
         return type_info_cache[type_id]
     
     # 处理每个物品的材料数据
     for type_id, type_data in yaml_data.items():
         if 'materials' in type_data:
             # 获取物品的信息
-            type_info_cache = get_type_info(type_id)
-            category_id = type_info_cache['categoryid']
-            process_size = type_info_cache['process_size']
+            type_info = get_type_info(type_id)
+            category_id = type_info['categoryid']
+            process_size = type_info['process_size']
             
             for material in type_data['materials']:
                 material_type_id = material['materialTypeID']
                 quantity = material['quantity']
-                material_data = get_material_info(material_type_id)
+                material_info = get_type_info(material_type_id)
                 
                 # 逐行插入数据
                 cursor.execute(
                     '''INSERT OR REPLACE INTO typeMaterials 
-                       (typeid, categoryid, process_size, output_material, output_quantity, output_material_name, output_material_icon) 
-                       VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                    (type_id, category_id, process_size, material_type_id, quantity, material_data['name'], material_data['icon'])
+                       (typeid, categoryid, process_size, output_material, output_material_categoryid, output_quantity, output_material_name, output_material_icon) 
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+                    (type_id, category_id, process_size, material_type_id, material_info['categoryid'], quantity, material_info['name'], material_info['icon'])
                 )
