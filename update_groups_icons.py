6ea053be6d533e982dbc3505278bb7d4b14a9a3b
GG <46676047+EstamelGG@users.noreply.github.com>
2025-03-24 14:54:04 +0800

    update

--- update_groups_icons.py
+++ update_groups_icons.py
@@ -2,51 +2,20 @@ import sqlite3
 
 def update_groups_with_icon_filename(cursor):
     """根据 group_id 从 types 表获取 icon_filename，并更新 groups 表"""
-    # 获取 groups 表中的所有 group_id
-    cursor.execute('SELECT group_id FROM groups')
-    groups = cursor.fetchall()
-    
-    # 创建一个缓存字典来存储已查询的图标
-    icon_cache = {}
-    
-    # 批量更新列表
-    updates = []
-    
-    for group in groups:
-        group_id_value = group[0]
-        
-        # 如果图标已在缓存中，直接使用
-        if group_id_value in icon_cache:
-            icon_filename = icon_cache[group_id_value]
-        else:
-            # 获取对应的 icon_filename，查找符合条件的第一条数据
-            cursor.execute('''
-                SELECT icon_filename 
-                FROM types 
-                WHERE groupID = ? 
-                  AND icon_filename != "items_73_16_50.png" 
-                  AND icon_filename != "items_7_64_15.png" 
-                  AND icon_filename != "icon_0_64.png" 
-                ORDER BY metaGroupID ASC 
-                LIMIT 1
-            ''', (group_id_value,))
-            result = cursor.fetchone()
-            
-            if result:
-                icon_filename = result[0]
-            else:
-                icon_filename = "items_73_16_50.png"  # 如果没有找到符合条件的结果，设置为默认值
-            
-            # 将结果存入缓存
-            icon_cache[group_id_value] = icon_filename
-        
-        # 添加到批量更新列表
-        updates.append((icon_filename, group_id_value))
-    
-    # 批量执行更新
-    cursor.executemany('''
+    # 使用单个JOIN查询获取所有需要的数据
+    cursor.execute('''
+        WITH RankedIcons AS (
+            SELECT 
+                t.groupID,
+                t.icon_filename,
+                ROW_NUMBER() OVER (PARTITION BY t.groupID ORDER BY t.metaGroupID) as rn
+            FROM types t
+            WHERE t.icon_filename NOT IN ("items_73_16_50.png", "items_7_64_15.png", "icon_0_64.png")
+        )
         UPDATE groups
-        SET icon_filename = ?
-        WHERE group_id = ?
-    ''', updates)
+        SET icon_filename = COALESCE(
+            (SELECT icon_filename FROM RankedIcons WHERE groupID = groups.group_id AND rn = 1),
+            "items_73_16_50.png"
+        )
+    ''')
 
