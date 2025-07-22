e0a035d624d280a631301fff0c7fcca823d732e9
GG <46676047+EstamelGG@users.noreply.github.com>
2025-07-22 16:40:21 +0800

    update

--- update_groups_icons.py
+++ update_groups_icons.py
@@ -8,14 +8,21 @@ def update_groups_with_icon_filename(cursor):
             SELECT 
                 t.groupID,
                 t.icon_filename,
-                ROW_NUMBER() OVER (PARTITION BY t.groupID ORDER BY t.metaGroupID) as rn
+                ROW_NUMBER() OVER (
+                    PARTITION BY t.groupID 
+                    ORDER BY 
+                        CASE WHEN t.published = 1 THEN 0 ELSE 1 END,  -- 优先已发布
+                        t.metaGroupID                                -- 然后按 metaGroupID 升序
+                ) AS rn
             FROM types t
             WHERE t.icon_filename NOT IN ("items_73_16_50.png", "items_7_64_15.png", "icon_0_64.png")
         )
         UPDATE groups
         SET icon_filename = COALESCE(
-            (SELECT icon_filename FROM RankedIcons WHERE groupID = groups.group_id AND rn = 1),
+            (SELECT icon_filename 
+             FROM RankedIcons 
+             WHERE groupID = groups.group_id AND rn = 1),
             "items_73_16_50.png"
-        )
+        );
     ''')
 
